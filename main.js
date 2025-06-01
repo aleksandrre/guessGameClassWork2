30abb1f3c7b01fe6567575d4c9597bb21245f1b6
chokss <alekochokheli.01@gmail.com>
2025-06-01 14:35:06 +0400

    add some func

--- main.js
+++ main.js
@@ -3,10 +3,17 @@ boxContainer.classList.add("boxContainer");
 document.body.appendChild(boxContainer);
 const allBoxes = [];
 const allBoxValues = [];
+
 const createBox = () => {
+  let randomNumber;
+
+  do {
+    randomNumber = Math.floor(Math.random() * 10) + 1;
+  } while (allBoxValues.includes(randomNumber));
+
   const box = document.createElement("div");
   const boxValue = document.createElement("p");
-  const randomNumber = Math.floor(Math.random() * 10) + 1;
+
   allBoxValues.push(randomNumber);
   boxValue.textContent = randomNumber;
 
@@ -18,25 +25,30 @@ const createBox = () => {
   allBoxes.push(box);
 };
 
-const createManyBox = () => {
-  for (let i = 0; i < 3; i++) {
+const createManyBox = (boxQuantity) => {
+  for (let i = 0; i < boxQuantity; i++) {
     createBox();
   }
 };
 
-createManyBox();
+createManyBox(3);
 
-const guessedNumber = () => {
-  const guessedNumber = document.createElement("p");
-  document.body.appendChild(guessedNumber);
-  guessedNumber.textContent = allBoxValues[Math.floor(Math.random() * 3)];
-};
-guessedNumber();
+const message = document.getElementById("message");
+const guessedNumber = document.createElement("p");
+document.body.appendChild(guessedNumber);
+guessedNumber.textContent = allBoxValues[Math.floor(Math.random() * 3)];
 
 const chooseOneBox = () => {
   allBoxes.forEach((box) => {
     box.addEventListener("click", () => {
-      box.querySelector(".boxValue").style.display = "block";
+      box.querySelector(".boxValue").style.opacity = 1;
+      if (
+        box.querySelector(".boxValue").textContent == guessedNumber.textContent
+      ) {
+        message.textContent = "answer is right";
+      } else {
+        message.textContent = "answer is false";
+      }
     });
   });
 };
