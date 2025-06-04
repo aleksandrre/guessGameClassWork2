75a741412aac839514fb4d32956501d2517f2801
chokss <alekochokheli.01@gmail.com>
2025-06-04 18:17:51 +0400

    add some

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const boxContainer = document.createElement("div");
+const guessedNumberP = document.createElement("p");
+
+const gameContainer = document.querySelector(".gameContainer");
+const message = document.getElementById("message");
+
 boxContainer.classList.add("boxContainer");
-document.body.appendChild(boxContainer);
+guessedNumberP.classList.add("guessedNumberP");
+gameContainer.appendChild(boxContainer);
+
 const allBoxes = [];
 const allBoxValues = [];
 
@@ -31,26 +38,60 @@ const createManyBox = (boxQuantity) => {
   }
 };
 
-createManyBox(3);
 
-const message = document.getElementById("message");
-const guessedNumber = document.createElement("p");
-document.body.appendChild(guessedNumber);
-guessedNumber.textContent = allBoxValues[Math.floor(Math.random() * 3)];
+
+let guessedNumber;
+
+const displayGuessedNumberP = () => {
+  guessedNumber = allBoxValues[Math.floor(Math.random() * allBoxValues.length)];
+  gameContainer.appendChild(guessedNumberP);
+  guessedNumberP.textContent = guessedNumber;
+};
+
+let choice = 2;
 
 const chooseOneBox = () => {
   allBoxes.forEach((box) => {
     box.addEventListener("click", () => {
       box.querySelector(".boxValue").style.opacity = 1;
-      if (
-        box.querySelector(".boxValue").textContent == guessedNumber.textContent
-      ) {
-        message.textContent = "answer is right";
+      if (box.querySelector(".boxValue").textContent == guessedNumber) {
+        message.textContent = "Answer Is Right";
+        resetFunction();
       } else {
-        message.textContent = "answer is false";
+        choice-- 
+          if (choice <= 0) {
+          message.textContent = "წააგე ჰეჰე."
+          resetFunction()
+        }else{
+          message.textContent = "Answer Is False but you can do it!";
+    
+
+        }
       }
     });
   });
 };
 
-chooseOneBox();
+
+
+const mainFunction = () =>{
+  createManyBox(3);
+  displayGuessedNumberP();
+  chooseOneBox();
+  
+}
+
+mainFunction()
+
+
+const resetFunction = () =>{ 
+ setTimeout(() => {
+  boxContainer.innerHTML="";
+  message.textContent="";
+  guessedNumberP.textContent="";
+  allBoxValues.length=0;
+  allBoxes.lenght=0;
+  choice=2;
+  mainFunction()
+ }, 2000);
+}
